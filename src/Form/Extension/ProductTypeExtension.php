3c3cedb246eacc7b393435c7e3ac104f77e6e6b4
Maciej Hejduk <maciej.wisebadgers@gmail.com>
2022-07-27 15:46:18 +0200

    Overwrite _details.html.twig template for Product form

--- src/Form/Extension/ProductTypeExtension.php
+++ src/Form/Extension/ProductTypeExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Form\Extension;
 
+use Sylius\Bundle\ProductBundle\Form\Type\ProductType;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,15 +13,17 @@ class ProductTypeExtension extends AbstractTypeExtension
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('colors', ChoiceType::class, [
-            'red' => 'red',
-            'blue' => 'blue',
-            'green' => 'green'
+        $builder->add('color', ChoiceType::class, [
+            'choices' => [
+                'red' => 'red',
+                'blue' => 'blue',
+                'green' => 'green'
+            ]
         ]);
     }
 
     public static function getExtendedTypes(): iterable
     {
-        return [\Sylius\Bundle\CoreBundle\Form\Extension\ProductTypeExtension::class];
+        return [ProductType::class];
     }
 }
